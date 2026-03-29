caa31abb3c13001c2f903734d5c048e09e3c5ccd
sai-rohith-bv <rohithgsal1@gmail.com>
2026-03-29 11:28:21 +0530

    Create edit route and update route for editing chats using method-override

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const port=9099;
 const path=require("path");
 const mongoose=require("mongoose");
 const chat=require("./models/chats");
+const methodoverride=require("method-override");
 console.log("welcome to app");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extented:true}));
+app.use(methodoverride("_method"));
 main().then(()=>{
     console.log("Connection Successfull!");
 }).catch(err => console.log(err));
@@ -27,12 +29,9 @@ chat1.save().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});
 
 app.get("/chats",async(req,res)=>{
    let chats= await chat.find();//async function to get data it returns promise 
-   console.log(chats);
      res.render("index",{chats});
 });
-app.get("/",(req,res)=>{
-   res.send("Working");
-});
+
 app.post("/chats",(req,res)=>{
          let {from,msg,to}=req.body;
          let newchat=new chat(
@@ -49,8 +48,21 @@ app.post("/chats",(req,res)=>{
 });
 app.get("/chats/new",(req,res)=>{
     res.render("new");
+});
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    let cha=await chat.findById(id);
+    res.render("edit",{cha});
 })
+app.put("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let {msg:newmsg}=req.body;//here we have to chance origunal msg in schema to newmsg
+    let updatechat= await chat.findByIdAndUpdate(id,{msg:newmsg},{runvalidators:true,new:true});
+     res.redirect("/chats");
+})
+app.get("/",(req,res)=>{
+   res.send("Working");
+});
 app.listen(port,()=>{
     console.log(`server running on ${port}...`);
 })
-
